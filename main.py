50a951db342ac44bce69b3f923532eb8f9c53d6c
Saxda <96083920+Saxda@users.noreply.github.com>
2022-06-20 21:32:47 +0200

    changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,4 +9,4 @@ def homepage():
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
-
+"""
